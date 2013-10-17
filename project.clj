1d7e4c8c326324f251bf9386d433c5aaac7adfad
Mitchell Kember <mk12360@gmail.com>
2013-10-17 00:27:47 -0400

    Second commit

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject mini-pinions "0.1.0-SNAPSHOT"
+(defproject mk12/mini-pinions "0.1.0"
   :description "ICS4U first summative project"
   :url "http://github.com/mk12/mini-pinions"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [quil "1.6.0"]]
-  :main mini-pinions.core
+  :main ^:skip-aot mini-pinions.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mini_pinions/core.clj
+++ src/mini_pinions/core.clj
@@ -1,7 +1,21 @@
 (ns mini-pinions.core
-  (:gen-class))
+  (:use quil.core))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def pos (atom 0))
+
+(defn setup []
+  (smooth)
+  (background 200))
+
+(defn draw []
+  (swap! pos inc)
+  (ellipse (/ (width) 2) @pos 20 20))
+
+(defsketch main-sketch
+  :title "Mini Pinions"
+  :setup setup
+  :draw draw
+  :size [500 500])
+
+(defn -main [& args]
+  (main-sketch))
